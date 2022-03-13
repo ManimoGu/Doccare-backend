a9ba832b1e60d565981b173b5540db9c7d911029
ManimoGu <guennach.imane@gmail.com>
2022-03-13 21:23:14 +0100

    organizimg

---
+++ API/Patient.js
@@ -0,0 +1,21 @@
+const { sqlQuery } = require("../Helpers/Promise");
+const randomstring = require("randomstring");
+
+
+exports.PatientDash = async (res,err) =>{
+
+
+}
+
+exports.PatientList = async (res,err) => {
+
+
+}
+
+
+exports.PatientNbr = async (res,err) => {
+
+
+}
+
+
