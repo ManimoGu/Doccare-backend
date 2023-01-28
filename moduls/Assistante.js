659fbca3de80d665997143daa0d0bdbbd0bdc5e0
ManimoGu <guennach.imane@gmail.com>
2023-01-28 23:26:50 +0100

    last

--- moduls/Assistante.js
+++ moduls/Assistante.js
@@ -1,22 +1,27 @@
 class Assistante {
-
-    constructor(Nom='', Prénom='', Date_naissance,  CIN='', Tel='', Adresse='', Avatar = '', Entree_le ,Cabinet, Account){
-
-        this.Nom = Nom;
-        this.Prénom = Prénom;
-        this.Date_naissance = Date_naissance;
-        this.CIN = CIN;
-        this.Tel = Tel;
-        this.Adresse = Adresse;
-        this.Avatar = Avatar;
-        this.Entree_le = Entree_le;
-        this.Cabinet = Cabinet;
-        this.Account = Account;
-       
-
-
-    }
-
+  constructor(
+    Nom = "",
+    Prénom = "",
+    Date_naissance,
+    CIN = "",
+    Tel = "",
+    Adresse = "",
+    Avatar = "",
+    Entree_le,
+    Cabinet,
+    Account
+  ) {
+    this.Nom = Nom;
+    this.Prénom = Prénom;
+    this.Date_naissance = Date_naissance;
+    this.CIN = CIN;
+    this.Tel = Tel;
+    this.Adresse = Adresse;
+    this.Avatar = Avatar;
+    this.Entree_le = Entree_le;
+    this.Cabinet = Cabinet;
+    this.Account = Account;
+  }
 }
 
-exports.Assistante = Assistante 
+exports.Assistante = Assistante;
