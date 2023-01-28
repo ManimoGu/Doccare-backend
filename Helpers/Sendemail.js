33a5cf6bbcedfccc1434fb00a0a5f058dc7a73e3
ManimoGu <guennach.imane@gmail.com>
2023-01-28 22:55:08 +0100

    last

--- Helpers/Sendemail.js
+++ Helpers/Sendemail.js
@@ -1,35 +1,35 @@
-const { mailgun } = require("../Config/email");
-const nodemailer = require("nodemailer");
+// const { mailgun } = require("../Config/email");
+// const nodemailer = require("nodemailer");
 
 
 
 
-exports.SendEmail = async (EmailInfo) => {
+// exports.SendEmail = async (EmailInfo) => {
 
-    let transporter = nodemailer.createTransport({
-        host: "mailgun.org",
-        port: 587,
-        secure : false, // true for 465, false for other ports
-        auth: {
-          user: mailgun.usename, // generated ethereal user
-          pass: mailgun.password, // generated ethereal password
-        },
-      });
+//     let transporter = nodemailer.createTransport({
+//         host: "mailgun.org",
+//         port: 587,
+//         secure : false, // true for 465, false for other ports
+//         auth: {
+//           user: mailgun.usename, // generated ethereal user
+//           pass: mailgun.password, // generated ethereal password
+//         },
+//       });
 
-      let message = {
-        from: EmailInfo.from, // sender address
-        to: EmailInfo.to, // list of receivers
-        subject: EmailInfo.subject, // Subject line
-        html: EmailInfo.message
-        // html body
-        ,
-        tls:{ 
-          rejectUnauthorized:false
-        }
-      };
+//       let message = {
+//         from: EmailInfo.from, // sender address
+//         to: EmailInfo.to, // list of receivers
+//         subject: EmailInfo.subject, // Subject line
+//         html: EmailInfo.message
+//         // html body
+//         ,
+//         tls:{ 
+//           rejectUnauthorized:false
+//         }
+//       };
 
-      //send the email
+//       //send the email
 
-      let info = await transporter.sendMail(message)
-      console.log(info)
-}
+//       let info = await transporter.sendMail(message)
+//       console.log(info)
+// }
