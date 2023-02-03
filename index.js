9a098ae486dac74779fe2ee644957b4244581c3d
ManimoGu <guennach.imane@gmail.com>
2023-02-03 08:52:08 +0100

    header res

--- index.js
+++ index.js
@@ -1,156 +1,212 @@
-
-
-const express = require("express")
-// const cors = require("cors")
-const bodyparser = require("body-parser")
-const { register, verify, Signin, resend, forgot, resetPass, reseSettingtPass, UploadFile} = require("./API/auth");
-const { DoctorProfil, AddDoctor, DeleteDoctor, UpdateDoctor, DoctorList, UpdateAvatarDocteur } = require("./API/doctor");
-const { AddAssistante, DeleteAssistante, UpdateAssistante, AssistanteList, UpdateAvatarAssistante } = require("./API/Assistante");
-const { PatientNbr, PatientList, AddPatient, UpdatePatient, DeletePatient, Patient_consultations, PatientFiles } = require("./API/Patient");
-const { ReviewsNbr, ListMessage, AddResp, getResponse } = require("./API/Reviews");
-const { ConsultationNbr, ConsultationTotal, DashList, ConsultationList, UpdateConsultation } = require("./API/Consultations");
-const { ADDRDV, RDVList, DeleteRDV, UpdateRDV, RDVNbr, TypeUpdate} = require("./API/RDV");
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const {
+  register,
+  verify,
+  Signin,
+  resend,
+  forgot,
+  resetPass,
+  reseSettingtPass,
+  UploadFile,
+} = require("./API/auth");
+const {
+  DoctorProfil,
+  AddDoctor,
+  DeleteDoctor,
+  UpdateDoctor,
+  DoctorList,
+  UpdateAvatarDocteur,
+} = require("./API/doctor");
+const {
+  AddAssistante,
+  DeleteAssistante,
+  UpdateAssistante,
+  AssistanteList,
+  UpdateAvatarAssistante,
+} = require("./API/Assistante");
+const {
+  PatientNbr,
+  PatientList,
+  AddPatient,
+  UpdatePatient,
+  DeletePatient,
+  Patient_consultations,
+  PatientFiles,
+} = require("./API/Patient");
+const {
+  ReviewsNbr,
+  ListMessage,
+  AddResp,
+  getResponse,
+} = require("./API/Reviews");
+const {
+  ConsultationNbr,
+  ConsultationTotal,
+  DashList,
+  ConsultationList,
+  UpdateConsultation,
+} = require("./API/Consultations");
+const {
+  ADDRDV,
+  RDVList,
+  DeleteRDV,
+  UpdateRDV,
+  RDVNbr,
+  TypeUpdate,
+} = require("./API/RDV");
 const fileupload = require("express-fileupload");
 
 const { Access } = require("./Helpers/JwtVerification");
-//create an app 
+//create an app
 
 const app = express();
 
 app.use(fileupload());
 
-app.use(express.json())
-
+app.use(express.json());
 
 //enable Listening http server
 
-const port = process.env.PORT || 9000
-
-
+const port = process.env.PORT || 9000;
 
 app.listen(port, (req, resp) => {
-    console.log("Server is runing on port 9000..." + __dirname);
-  });
+  console.log("Server is runing on port 9000..." + __dirname);
+});
 
-  app.use(bodyparser.urlencoded({limit: "50mb", extended : true, parameterLimit : 50000}))
+app.use(
+  bodyparser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 //looks at requests where the content-type : application-json (header).
-  app.use(bodyparser.json())
+app.use(bodyparser.json());
 
-//   const corsOptions = {
-//     origin: '*',
-//     optionsSuccessStatus: 200,
-//     credentials: true
-    
-// }
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
-  // app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-  app.use(express.static('API'))
+app.use(express.static("API"));
 
-  
-// app.use((req,res,next)=>{
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// })
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-  app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-    res.send("hello")
+// Signin API
 
-  })
+app.post("/api/auth/register", register);
 
+app.get("/api/verify-email/:login/code/:token", verify);
 
-  // Signin API
+app.get("/api/Signin/:login/pass/:password", DoctorProfil);
 
-  app.post("/api/auth/register", register);
+app.get("/api/resend/:login/code/:token", resend);
 
-  app.get("/api/verify-email/:login/code/:token", verify);
+app.post("/api/fogot_password/:login", forgot);
 
-  app.get("/api/Signin/:login/pass/:password", DoctorProfil);
+app.post("/api/resetpassword/:login/code/:token", resetPass);
 
-  app.get("/api/resend/:login/code/:token", resend);
+app.post(
+  "/api/resetpasswordSetting/:login/code/:password",
+  Access,
+  reseSettingtPass
+);
 
-  app.post("/api/fogot_password/:login",forgot)
+app.post(
+  "/api/UploadFile/Nom/:Nom/Prenom/:Prenom/type/:type",
+  Access,
+  UploadFile
+);
 
-  app.post("/api/resetpassword/:login/code/:token", resetPass)
+// Doctors API
 
-  app.post("/api/resetpasswordSetting/:login/code/:password",Access, reseSettingtPass)
+app.post("/api/Doctor/DoctorProfil/:login/password/:password", DoctorProfil);
 
-  app.post("/api/UploadFile/Nom/:Nom/Prenom/:Prenom/type/:type",Access, UploadFile)
+app.get("/api/Doctor/AddDoctor/:cabinet", Access, AddDoctor);
 
-  // Doctors API 
+app.get("/api/Doctor/DeleteDoctor/:Id", Access, DeleteDoctor);
 
-  app.post("/api/Doctor/DoctorProfil/:login/password/:password", DoctorProfil);
+app.post("/api/Doctor/UpdateDoctorCabinet", Access, UpdateDoctor);
 
-  app.get("/api/Doctor/AddDoctor/:cabinet",Access, AddDoctor);
+app.get("/api/Doctor/DoctorList/id/:id", Access, DoctorList);
 
-  app.get("/api/Doctor/DeleteDoctor/:Id",Access, DeleteDoctor);
-  
-  app.post("/api/Doctor/UpdateDoctorCabinet",Access, UpdateDoctor);
+app.put("/api/Doctor/DoctorList/UpdatePic/id/:id", Access, UpdateAvatarDocteur);
 
-  app.get("/api/Doctor/DoctorList/id/:id",Access, DoctorList);
+// Assistante API
 
+app.post("/api/Doctor/AddAssistante/id/:cabinet", Access, AddAssistante);
 
-  app.put("/api/Doctor/DoctorList/UpdatePic/id/:id",Access, UpdateAvatarDocteur);
+app.delete("/api/Doctor/DeleteAssistante/id/:id", Access, DeleteAssistante);
 
+app.put("/api/Doctor/UpdateAssistante/id/:id", Access, UpdateAssistante);
 
-  // Assistante API 
+app.get("/api/Doctor/AssistanteList/id/:id", Access, AssistanteList);
 
-  app.post("/api/Doctor/AddAssistante/id/:cabinet",Access, AddAssistante);
+app.put(
+  "/api/Doctor/AssistanteList/ChangePic/id/:id",
+  Access,
+  UpdateAvatarAssistante
+);
 
-  app.delete("/api/Doctor/DeleteAssistante/id/:id",Access, DeleteAssistante);
-  
-  app.put("/api/Doctor/UpdateAssistante/id/:id",Access,UpdateAssistante);
+// Patient API
 
-  app.get("/api/Doctor/AssistanteList/id/:id",Access, AssistanteList);
+app.get("/api/Doctor/NombredePatient/id/:id", Access, PatientNbr);
+app.get("/api/Doctor/DashboardList/id/:id", Access, DashList);
+app.get("/api/Doctor/PatientsList/id/:id", Access, PatientList);
+app.post("/api/Doctor/AddPatient/id/:id", Access, AddPatient);
+app.put("/api/Doctor/UpdatePatient/id/:id", Access, UpdatePatient);
+app.put("/api/Doctor/DeletePatient/id/:id/:Account", Access, DeletePatient);
+app.get(
+  "/api/Doctor/Patient_fiche_consultation/Cabinet/:idCabinet/Patient/:idPatient",
+  Access,
+  Patient_consultations
+);
+app.put(
+  "/api/Doctor/Patient_fiche_consultation/AddFile/idFiche/:idFiche",
+  PatientFiles
+);
 
-  app.put("/api/Doctor/AssistanteList/ChangePic/id/:id",Access, UpdateAvatarAssistante);
+// Feedback API
 
+app.get("/api/Doctor/NombredAvis/id/:id", Access, ReviewsNbr);
+app.get("/api/Doctor/ListMessage/id/:id", Access, ListMessage);
+app.post("/api/Doctor/AddResponse/id/:id", Access, AddResp);
+app.get("/api/Doctor/GetResponse/id/:id", Access, getResponse);
 
-  // Patient API 
+// Consultation API
 
-  app.get("/api/Doctor/NombredePatient/id/:id",Access, PatientNbr);
-  app.get("/api/Doctor/DashboardList/id/:id",Access, DashList);
-  app.get("/api/Doctor/PatientsList/id/:id",Access,PatientList);
-  app.post("/api/Doctor/AddPatient/id/:id",Access, AddPatient);
-  app.put("/api/Doctor/UpdatePatient/id/:id",Access, UpdatePatient);
-  app.put("/api/Doctor/DeletePatient/id/:id/:Account",Access, DeletePatient);
-  app.get("/api/Doctor/Patient_fiche_consultation/Cabinet/:idCabinet/Patient/:idPatient",Access, Patient_consultations);
-  app.put("/api/Doctor/Patient_fiche_consultation/AddFile/idFiche/:idFiche", PatientFiles)
+app.get("/api/Doctor/NombreConsultation/id/:id", Access, ConsultationNbr);
+app.get("/api/Doctor/TotalConsultation/id/:id", Access, ConsultationTotal);
+app.get("/api/Doctor/ListConsultation/id/:id", Access, ConsultationList);
+app.put("/api/Doctor/UpdateConsultation/id", Access, UpdateConsultation);
 
+// RVD API
 
-
-  // Feedback API
-
-  app.get("/api/Doctor/NombredAvis/id/:id",Access, ReviewsNbr);
-  app.get("/api/Doctor/ListMessage/id/:id",Access, ListMessage);
-  app.post ("/api/Doctor/AddResponse/id/:id",Access, AddResp);
-  app.get ("/api/Doctor/GetResponse/id/:id",Access, getResponse);
-
-
-
-  // Consultation API 
-
-
-app.get("/api/Doctor/NombreConsultation/id/:id",Access, ConsultationNbr);
-app.get("/api/Doctor/TotalConsultation/id/:id",Access,ConsultationTotal);
-app.get("/api/Doctor/ListConsultation/id/:id",Access, ConsultationList);
-app.put("/api/Doctor/UpdateConsultation/id",Access, UpdateConsultation);
- 
-
-  // RVD API 
-
-  app.put("/api/DoctorAssistante/AddRDV/id/:id",Access, ADDRDV);
-  app.get("/api/DoctorAssistante/ListRDV/id/:id",Access, RDVList);
-  app.delete("/api/DoctorAssistante/DeleteRDV/id/:id",Access, DeleteRDV);
-  app.put("/api/DoctorAssistante/UpdateRDV/id/:id",Access, UpdateRDV);
-  app.get("/api/DoctorAssistante/NBRRDV/id/:id",Access, RDVNbr);
-  app.get("/api/DoctorAssistante/UpdateType/id/:id/Patient/:Patient",Access, TypeUpdate);
-
-
-
-
-
+app.put("/api/DoctorAssistante/AddRDV/id/:id", Access, ADDRDV);
+app.get("/api/DoctorAssistante/ListRDV/id/:id", Access, RDVList);
+app.delete("/api/DoctorAssistante/DeleteRDV/id/:id", Access, DeleteRDV);
+app.put("/api/DoctorAssistante/UpdateRDV/id/:id", Access, UpdateRDV);
+app.get("/api/DoctorAssistante/NBRRDV/id/:id", Access, RDVNbr);
+app.get(
+  "/api/DoctorAssistante/UpdateType/id/:id/Patient/:Patient",
+  Access,
+  TypeUpdate
+);
